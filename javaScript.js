e9dffed0dd23603990b8fc40d6319d12ce001794
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-30 23:19:00 +0300

    PLAY ONE ROUND OF THE GAME AND GET THE RESULT
    Function will get the player and the computer
    selection and determine the winner

--- javaScript.js
+++ javaScript.js
@@ -10,12 +10,52 @@ function getComputerChoice()
     else if(random==1)
     {
         console.log("Paper");
-        choice="rock";
+        choice="paper";
     }
     else if(random==2)
     {
         console.log("Scissors");
-        choice="rock";
+        choice="scissors";
     }
     return choice;
 }
+
+
+function playRound(playerSelection,computerSelection)
+{  
+    let result;
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection===computerSelection)
+    {
+      result="Its a tie!"
+    }
+    else if(playerSelection=="rock"&& computerSelection=="paper")
+    {
+        result="You Lose! Paper beats Rock";
+    }
+    else if(playerSelection=="rock"&& computerSelection=="scissors")
+    {
+        result="You Win! Rock beats Scissors";
+    }
+    else if(playerSelection=="paper"&& computerSelection=="rock")
+    {
+        result="You Win! Paper beats Rock";
+    }
+    else if(playerSelection=="paper"&& computerSelection=="scissors")
+    {
+        result="You Lose! Scissors beats Paper";
+    }
+    else if(playerSelection=="scissors"&& computerSelection=="paper")
+    {
+        result="You Win! Scissors beats Paper";
+    }
+    else if(playerSelection=="scissors"&& computerSelection=="rock")
+    {
+        result="You Lose! Rock beats Scissors";
+    }
+    return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
